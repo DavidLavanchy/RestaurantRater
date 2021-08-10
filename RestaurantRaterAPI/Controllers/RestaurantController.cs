43240c602939b9ab1225056325bf4991c22e2e9a
David Lavanchy <dplavanchy@gmail.com>
2021-08-09 21:27:39 -0400

    finished out get methods

---
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,66 @@
+﻿using RestaurantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+
+        //POST (create)
+        //api/Restaurant
+        [HttpPost]
+        public async Task<IHttpActionResult> PostRestaurant([FromBody] Restaurant model)
+        {
+            if (model is null)
+            {
+                return BadRequest("Request body cannot be empty.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                //Store model in database
+                _context.Restaurants.Add(model);
+                int changeCount = await _context.SaveChangesAsync();
+                return Ok("Your Restaurant was created!");
+            }
+
+            //if not valid, then reject it
+            return BadRequest(ModelState);
+        }
+
+        // Get ALL
+        // api/Restaurant
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+
+            return Ok(restaurants);
+        }
+
+
+        // Get by ID
+        //api.Restaurant/{id}
+        [HttpGet]
+        public async Task<IHttpActionResult> GetByID(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+            return NotFound();
+        }
+
+
+    }
+}
