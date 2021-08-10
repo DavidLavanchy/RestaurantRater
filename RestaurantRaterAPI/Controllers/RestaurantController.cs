0f488041f784ae50cc01c0ed35a36f47b79a8ece
David Lavanchy <dplavanchy@gmail.com>
2021-08-09 21:47:58 -0400

    delete method

--- RestaurantRaterAPI/Controllers/RestaurantController.cs
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -61,6 +61,61 @@ namespace RestaurantRaterAPI.Controllers
             return NotFound();
         }
 
+        // Put (update)
+        // api/Restaurant/{id}
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRestaurantmethod([FromUri] int id, [FromBody] Restaurant updatedRestaraunt)
+        {
+            // Check if they match
+            if(id != updatedRestaraunt?.Id)
+            {
+                return BadRequest("Id's do not match");
+            }
 
+            //check model state
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            //find restaraunt in database
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+            //if restaurant doesn't exist
+            if(restaurant is null)
+            {
+                return NotFound();
+            }
+            //update properties
+            restaurant.Name = updatedRestaraunt.Name;
+            restaurant.Address = updatedRestaraunt.Address;
+            restaurant.Rating = updatedRestaraunt.Rating;
+
+            //save the change
+            await _context.SaveChangesAsync();
+
+            return Ok("Restaraunt was updated.");
+        }
+
+        //Delete (delete)
+        // api/Restaurant/{id}
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteRestaurant([FromUri]int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if(restaurant is null)
+            {
+                return NotFound();
+            }
+
+            _context.Restaurants.Remove(restaurant);
+
+            if(await _context.SaveChangesAsync() == 1)
+            {
+                return Ok("Restaurant was deleted");
+            }
+            return InternalServerError();
+        }
     }
 }
